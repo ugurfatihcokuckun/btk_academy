35528b965899be3dc003d123b3260277b3efdc8a
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-06-23 01:25:41 +0300

    string_methods_test

--- test_string_methods/test_string_methods.py
+++ test_string_methods/test_string_methods.py
@@ -1,11 +1,44 @@
+website = "http://www.sadikturan.com"
+course = "Python Kursu: Bastan Sona Python Programlama Rehberiniz (40 saat)"
+
+
 # s1 = " Hello World "
 # s1 = s1.strip()
 # print(s1)
 
-# s2 = "www.sadikturan.com"
-# s2 = s2.replace("www.sadikturan.com","sadikturan")
-# print(s2)
+# s2 = "www.sadikturan.com".strip("w.moc")
 
-s3 = "COURSE"
-s3 = s3.lower()
-print(s3)
+# result = course.lower() s3
+
+# result = website.count("a") s4
+# result = website.count("www")
+# result = website.count("www",0,10) s4
+
+# result = website.startswith("www")
+# result = website.endswith("com") s5
+
+# result = website.find("com") 
+# result = website.find("com",0,10)
+# result = course.find("Python")
+# result = course.rfind("Python")
+# result = website.index("com")
+# result = website.rindex("com") s6
+
+# result = course.isalpha()
+# result = course.isdigit() s7
+
+# result= "contents".center(50,"*")
+# result= "contents".ljust(50,"*")
+# result= "contents".rjust(50,"*") s8
+
+# result = course.replace(" ","-" )
+# result = course.replace(" ","-",5) s9
+
+# result = "Hello World".replace("World", "There") s10
+
+result = course.split(" ")
+
+
+
+print(result)
+
