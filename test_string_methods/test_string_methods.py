cb0affec39444bc7951ded0f6cceff9956c407b4
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-06-23 00:54:52 +0300

    string_methods

---
+++ test_string_methods/test_string_methods.py
@@ -0,0 +1,11 @@
+# s1 = " Hello World "
+# s1 = s1.strip()
+# print(s1)
+
+# s2 = "www.sadikturan.com"
+# s2 = s2.replace("www.sadikturan.com","sadikturan")
+# print(s2)
+
+s3 = "COURSE"
+s3 = s3.lower()
+print(s3)
