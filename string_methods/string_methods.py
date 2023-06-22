5d3f3aed293c8969e318f8fc9e070b2a013defa0
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-06-23 00:39:38 +0300

    string_methods

---
+++ string_methods/string_methods.py
@@ -0,0 +1,25 @@
+message = "Hello There. My name is Sadik Turan"
+
+# message = message.upper() bütün karakterleri büyük karf yapar
+# message = message.lower() bütün karakterleri küçük hard yapar
+# message = message.title() bütün baş harfleri büyük harf yapar
+# message = message.capitalize() sadece baş harfi büyük yapar
+# message = message.strip() baştaki boşluk karakterini siler
+# message = message.split() boşluğu baz alarak parçalara ayırır
+# message = message.split('.') noktayı baz alarak parçalara ayırır 
+# message = ' '.join(message) ayrılmış elemanı birleştirir
+# message = '*'.join(message) araya '*' koyarak birleştirir
+# index = message.find('Sadik') kelimeyi cümle içerisinde arar ve bulduğu kelimenin ilk harfine göre index numarasını verir
+# isFound = message.startswith('H') 'H' harfi ile başlayıp başlamadığını sorar
+# isFound = message.endswith('n') 'n' karakteri ile bitip bitmediğini sorar
+# message = message.replace('Sadik','Çınar') verilen değişkeni deiştirir
+# message = message.center(50) verilen bilgiyi 50 karakterlik bir bilgi içerisine alır. Konteynır oluşturur
+# message = message.center(50,'*')  verilen bilgiyi 50 karakterlik bir bilgi içerisine alır ortalar ve boş karakterler yerine '*' koyar
+
+
+
+
+
+
+
+print(message)
