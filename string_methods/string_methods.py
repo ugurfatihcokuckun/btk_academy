50f6aaf80f1bcae75869ddcaaf671b3987c37917
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-06-23 00:43:42 +0300

    string_methods2

--- string_methods/string_methods.py
+++ string_methods/string_methods.py
@@ -17,7 +17,7 @@ message = "Hello There. My name is Sadik Turan"
 # message = message.center(50,'*')  verilen bilgiyi 50 karakterlik bir bilgi içerisine alır ortalar ve boş karakterler yerine '*' koyar
 
 
-
+asd
 
 
 
