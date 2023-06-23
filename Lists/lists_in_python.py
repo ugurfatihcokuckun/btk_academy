a4f4abc3c77e2278486be2e8b61d510c342d1b24
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-06-23 12:02:09 +0300

    lists

---
+++ Lists/lists_in_python.py
@@ -0,0 +1,21 @@
+# message = "Hello There. My name is Uğur Fatih Çokuçkun".split()
+# print(message)
+
+# my_list = [1, 2, 3]
+# my_list = ["bir", 2, True, 5.6]
+# print(my_list)
+
+# list1 = ["one", "two", "three"]
+# list2 = ["four", "five", "six"]
+
+# numbers = list1 + list2
+# print(numbers)
+# print(len(numbers))
+# print(numbers[2])
+
+userA = ["Fatih", 26]
+userB = ["Çınar", 2]
+users = [userA, userB]
+print(users)
+print(users[1])
+print(users[1][0])
