db9e4a19890f9b39085983e7bca2a4a25690de17
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-06-23 16:45:49 +0300

    tuple

---
+++ tuple/tuple.py
@@ -0,0 +1,24 @@
+list = [1, 2, 3]
+
+tuple = (1, "iki", 3)
+
+# print(type(tuple))
+# print(type(list))
+
+# print(len(tuple))
+
+list = ["ali", "veli"]
+tuple = ("Damla", "Ayşe", "Ayşe")
+names = ("Demet", "Emel", "Ayşe") + tuple
+
+
+# list[0] = "Ahmet"   *eleman değişim işlemi listede yapılabilir
+# tuple[0] = "Deniz"  *eleman değişim işlemi tupleda yapılamaz
+
+
+print(tuple.count("Ayşe"))
+
+
+print(list)
+print(tuple)
+print(names)
