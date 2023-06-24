eb741066216acbfe35d1be3f2b300bf149d909c2
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-06-24 23:40:16 +0300

    operator

---
+++ python_operatorleri/atama_operatorleri/atama_operatorleri.py
@@ -0,0 +1,34 @@
+# x = 5 
+# y = 10
+# z = 20
+
+# x,y,z = 5, 10 ,20
+
+# x,y = y,x
+
+# x = x + 5
+# x += 5
+# x *= 5
+# x /= 5
+# x %= 5
+# x //= 5
+# x **= 5
+
+# print(x,y,z)
+
+values = 1, 2, 3
+
+print(values)
+
+x ,y, z = values
+
+
+print(x,y,z)
+
+
+degerler = 1, 2, 3, 4, 5
+
+a, b, *c = degerler
+
+print(degerler)
+
