7363b29a42882e525f901092e0a5fab6e292eaf1
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-06-25 21:46:35 +0300

    kosul_ifadeleri

---
+++ if_else/Kosullu_durum/kosul_ifadeleri_2.py
@@ -0,0 +1,46 @@
+# number = int(input("Sayı girin: "))
+
+# if number > 0 and number < 100:
+#     print("Sayı 0-100 arasında")
+# else:
+#     print("Sayı 0-100 arasında değil")
+
+
+# number = int(input("sayı girin: "))
+
+# if number > 0 and number % 2 == 0:
+#     print("sayı çift ve pozitiftir")
+# else:
+#     print("sayı pozitif veya çift değildir")
+
+
+# email = input("mail girin: ")
+# password = input("password girin: ")
+
+# if email == "email@gmail.com":
+#     if password == "abc123":
+#         print("user")
+#     else:
+#         print("parola yanlış")
+# else:
+#     print("email hatalı")
+
+# a = int(input("sayıa girin: "))
+# b = int(input("sayıb girin: "))
+# c = int(input("sayıc girin: "))
+
+# if a > b and a > c:
+#     if b > c:
+#         print("a>b>c")
+#     elif c > b:
+#         print("a>c>b")
+# if b > c and b > a:
+#     if a > c:
+#         print("b>a>c")
+#     elif c > a:
+#         print("b>c>a")
+# if c > a and c > b:
+#     if a > b:
+#         print("c>a>b")
+#     elif b > a:
+#         print("c>b>a")
