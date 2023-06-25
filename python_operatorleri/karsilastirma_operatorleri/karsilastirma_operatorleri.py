35647e8c3bc028f0010409861a507a6a46bb58df
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-06-25 11:22:50 +0300

    operator

---
+++ python_operatorleri/karsilastirma_operatorleri/karsilastirma_operatorleri.py
@@ -0,0 +1,22 @@
+# username, password  => database
+
+
+
+a, b, c, d = 5, 5, 10, 4
+password = "1234"
+username = "sadikturan"
+
+result = a == b
+result = (a == c)
+result = a != b
+result = a >= b
+result = (True == 1)
+result = (False == 0)
+result = True + False + 40
+
+
+
+
+
+
+print(result)
