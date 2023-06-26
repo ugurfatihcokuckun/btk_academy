e7d4e462788ede7e1d23bfba5cf1d449b851d9ea
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-06-26 15:00:41 +0300

    break_continue

---
+++ döngüler/break_continue/break_continue.py
@@ -0,0 +1,35 @@
+"""
+name = "Fatih Çokuçkun"
+
+for letter in name:
+    if letter == "i":
+        continue
+    print(letter)
+"""
+
+
+"""
+x = 0 
+
+while x <5:
+    if x == 2:
+        break
+    print(x)
+    x += 1
+"""
+
+# 1'den yüze kadar tek sayıların toplamı
+
+
+"""
+x = 0
+result = 0
+
+while x <= 100:
+    x +=1
+    if x % 2 == 0:
+        continue
+    result += x
+    
+print(f"toplam {result}")
+"""
