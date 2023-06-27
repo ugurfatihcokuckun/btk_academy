b5c9f454f0aaeecb8a53bb51f9b7cc85036a5e9e
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-06-27 13:51:15 +0300

    global

---
+++ döngüler/fonksiyonlar/global_local.py
@@ -0,0 +1,62 @@
+# global scope
+x = "global x"
+
+def function():
+    # local scope
+    x = "local x"
+    print(x)
+     
+function()
+print(x)
+
+
+############
+
+name = "Fatih"
+
+def changeName(new_name):
+    name = new_name
+    print(name)
+
+changeName("Ada")
+print(name)
+
+############
+
+name = "global string"
+
+def greeting():
+    name = "Fatih"
+
+    def hello():
+        print("hello " + name)
+
+    hello()
+
+greeting()
+
+
+#############
+
+x = 50
+def test(x):
+    print(f"x = {x}")
+
+    x = 100
+    print(f"changed x to {x}")
+
+test(x)
+print(x)
+
+#############
+
+x = 50
+def test():
+    global x 
+    print(f"x = {x}")
+
+    x = 100
+    print(f"changed x to {x}")
+
+test()
+print(x)
