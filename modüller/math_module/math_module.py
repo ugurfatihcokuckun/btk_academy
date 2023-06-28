970bd9b4c9d0c451d905fbb1d3b6dcf95a5f8391
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-06-28 12:31:57 +0300

    module

---
+++ modüller/math_module/math_module.py
@@ -0,0 +1,24 @@
+## Yöntem 1
+# import math
+import math as islem
+
+# value = dir(math)
+# value = help(math)
+# value = help(math.factorial)
+# value = math.sqrt(49)
+# value = math.factorial(5)
+# value = math.floor(5.9)
+# value = math.ceil(5.9)
+
+# value = islem.factorial(5)
+
+## Yöntem 2
+# from math import *
+
+from math import factorial, sqrt
+
+value = factorial(5)
+value = sqrt(16)
+
+
+print(value)
