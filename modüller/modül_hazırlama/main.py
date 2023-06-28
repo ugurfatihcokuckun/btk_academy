8592f9aa3b4f60220a00706d7cd2824ba99821d4
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-06-28 18:41:39 +0300

    main

---
+++ modüller/modül_hazırlama/main.py
@@ -0,0 +1,15 @@
+import module_hazirlama
+
+# result = help(module_hazirlama)
+# result = help(module_hazirlama.func)
+result = module_hazirlama.number
+result = module_hazirlama.numbers
+result = module_hazirlama.person["name"]
+result = module_hazirlama.person["age"]
+result = module_hazirlama.func(10)
+
+p = module_hazirlama.Person()
+p.speak()
+
+
+print(result)
