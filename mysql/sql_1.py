2bdbfdd1d712d8f7a5a5b67d590455c3ca945693
ugurfatihcokuckun <ugurfatihcokuckun@gmail.com>
2023-08-27 13:15:44 +0300

    mysql

---
+++ mysql/sql_1.py
@@ -0,0 +1,63 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "377AqSwDe",
+    database="schooldb"
+)
+
+def insertProduct(name,price,imageUrl,description):
+    connection = mysql.connector.connect(host="localhost",user="root",password="377AqSwDe",database="node-app")
+    cursor = connection.cursor()
+
+    sql = "INSERT INTO products (name,price,imageUrl,description) VALUES (%s,%s,%s,%s)"
+    values = (name,price,imageUrl,description)
+
+    cursor.execute(sql,values)
+    try:
+        connection.commit()
+        print(f"{cursor.rowcount} tane kayıt eklendi")
+        print(f"son eklenen kaydın id:{cursor.lastrowid}")
+    except mysql.connector.Error as err:
+        print("hata",err)
+    finally:
+        connection.close()
+        print("database bağlantısı kapandı")
+
+
+def insertProducts(list):
+    connection = mysql.connector.connect(host="localhost",user="root",password="377AqSwDe",database="node-app")
+    cursor = connection.cursor()
+
+    sql = "INSERT INTO products (name,price,imageUrl,description) VALUES (%s,%s,%s,%s)"
+    values = list
+
+    cursor.executemany(sql,values)
+    try:
+        connection.commit()
+        print(f"{cursor.rowcount} tane kayıt eklendi")
+        print(f"son eklenen kaydın id:{cursor.lastrowid}")
+    except mysql.connector.Error as err:
+        print("hata",err)
+    finally:
+        connection.close()
+        print("database bağlantısı kapandı")
+
+list = []
+
+while True:
+    name = input("ürün adı: ")
+    price = input("ürün fiyatı: ")
+    imageUrl = input("ürün image: ")
+    description = input("ürün açıklaması: ")
+
+    list.append((name,price,imageUrl,description))
+
+    result = input("Devam etmek istiyor musunuz? (e/h)")
+    if result == "h":
+        print("Kayıtlarınız veri tabanına aktarılıyor...")
+        print(list)
+        insertProducts(list)
+        break
+#insertProduct(name,price,imageUrl,description)
